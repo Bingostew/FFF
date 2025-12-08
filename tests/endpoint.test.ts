da9c9f895e401f4498fbdc962fd06de87c78a055
Bingostew <m262940@usna.edu>
2025-12-07 20:47:36 -0500

    use static port

--- tests/endpoint.test.ts
+++ tests/endpoint.test.ts
@@ -2,8 +2,8 @@ import { describe, it, expect } from "vitest";
 
 describe("Initial endpoint", () => {
   it("should return 200 on initial request", async () => {
-    const res = await fetch("http://10.1.83.73:5174/");
+    const res = await fetch("http://localhost:5173/");
 
     expect(res.status).toBe(200);
-  });
+  })
 });
